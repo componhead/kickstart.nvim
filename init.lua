1112a8066f3558d791504d59276b144457cc9ea1
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-04-05 00:36:40 +0200

    add grep on quickfix find_files and grep_args

--- init.lua
+++ init.lua
@@ -415,6 +415,39 @@ require('lazy').setup({
               },
             },
           },
+          find_files = {
+            hidden = false,
+            mappings = {
+              i = {
+                ['<C-g>'] = {
+                  function(p_bufnr)
+                    -- send results to quick fix list
+                    require('telescope.actions').send_to_qflist(p_bufnr)
+                    local qflist = vim.fn.getqflist()
+                    local paths = {}
+                    local hash = {}
+                    for k in pairs(qflist) do
+                      local path = vim.fn.bufname(qflist[k]['bufnr']) -- extract path from quick fix list
+                      if not hash[path] then -- add to paths table, if not already appeared
+                        paths[#paths + 1] = path
+                        hash[path] = true -- remember existing paths
+                      end
+                    end
+                    -- show search scope with message
+                    vim.notify('find in ...\n  ' .. table.concat(paths, '\n  '))
+                    -- execute live_grep_args with search scope
+                    require('telescope').extensions.live_grep_args.live_grep_args { search_dirs = paths }
+                  end,
+                  type = 'action',
+                  opts = {
+                    nowait = true,
+                    silent = true,
+                    desc = 'Live grep on found files results',
+                  },
+                },
+              },
+            },
+          },
         },
         extensions = {
           ['ui-select'] = {
--- lua/custom/plugins/telescope-live-grep-args.lua
+++ lua/custom/plugins/telescope-live-grep-args.lua
@@ -17,14 +17,38 @@ return {
             n = {
               ['<Tab>'] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_worse,
               ['<S-Tab>'] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_better,
-              ['<C-s>'] = require('telescope-live-grep-args.actions').quote_prompt { postfix = ' ' },
-              ['<C-i>'] = require('telescope-live-grep-args.actions').quote_prompt { postfix = ' --iglob ' },
             },
             i = {
               ['<Tab>'] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_worse,
               ['<S-Tab>'] = require('telescope.actions').toggle_selection + require('telescope.actions').move_selection_better,
               ['<C-s>'] = require('telescope-live-grep-args.actions').quote_prompt { postfix = ' ' },
               ['<C-i>'] = require('telescope-live-grep-args.actions').quote_prompt { postfix = ' --iglob ' },
+              ['<C-g>'] = {
+                function(p_bufnr)
+                  -- send results to quick fix list
+                  require('telescope.actions').send_to_qflist(p_bufnr)
+                  local qflist = vim.fn.getqflist()
+                  local paths = {}
+                  local hash = {}
+                  for k in pairs(qflist) do
+                    local path = vim.fn.bufname(qflist[k]['bufnr']) -- extract path from quick fix list
+                    if not hash[path] then -- add to paths table, if not already appeared
+                      paths[#paths + 1] = path
+                      hash[path] = true -- remember existing paths
+                    end
+                  end
+                  -- show search scope with message
+                  vim.notify('find in ...\n  ' .. table.concat(paths, '\n  '))
+                  -- execute live_grep_args with search scope
+                  require('telescope').extensions.live_grep_args.live_grep_args { search_dirs = paths }
+                end,
+                type = 'action',
+                opts = {
+                  nowait = true,
+                  silent = true,
+                  desc = 'Live grep on results',
+                },
+              },
             },
           },
         },
