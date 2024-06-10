153e119ced34a009813c7f20324ba34e9acbd347
Emiliano Anichini <e.anichini@treedom.net>
2024-06-10 12:29:17 +0200

    remove git-conflict

--- init.lua
+++ init.lua
@@ -290,6 +290,7 @@ require('lazy').setup({
                   diffing_buffers = diffing_buffers + 1
                 end
               end
+              vim.api.cmd('diffing buffers', diffing_buffers)
               if diffing_buffers > 2 then
                 vim.keymap.set('n', '][', '', { silent = true, buffer = true })
                 vim.keymap.set('n', '[]', [[<cmd>diffget //1<CR>]czz]], { silent = true, desc = 'diffget base', buffer = true })
@@ -303,12 +304,6 @@ require('lazy').setup({
                   vim.keymap.set('n', '][', [[<cmd>diffget<CR>]czz]], { silent = true, desc = 'diffget other file', buffer = true })
                 end
               end
-              vim.keymap.set('n', ']x', '<Plug>(git-conflict-next-conflict)', { silent = true, desc = 'next conflict', buffer = true })
-              vim.keymap.set('n', '[x', '<Plug>(git-conflict-prev-conflict)', { silent = true, desc = 'previuos conflict', buffer = true })
-              vim.keymap.set('n', 'xo', '<Plug>(git-conflict-ours)', { silent = true, desc = 'get ours', buffer = true })
-              vim.keymap.set('n', 'xt', '<Plug>(git-conflict-theirs)', { silent = true, desc = 'get theirs', buffer = true })
-              vim.keymap.set('n', 'xb', '<Plug>(git-conflict-both)', { silent = true, desc = 'get both', buffer = true })
-              vim.keymap.set('n', 'x0', '<Plug>(git-conflict-none)', { silent = true, desc = 'get none', buffer = true })
             else
               vim.keymap.set({ 'n', 'v' }, '[r', gitsigns.reset_hunk, { desc = 'git reset hunk' })
               vim.keymap.set('n', ']r', gitsigns.stage_buffer, { desc = 'git stage buffer' })
