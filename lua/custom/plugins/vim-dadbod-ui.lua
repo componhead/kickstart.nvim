91eb451e1a058681befe89d1eabc3212cd24e2aa
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-10-02 11:54:52 +0200

    add db data

--- lua/custom/plugins/vim-dadbod-ui.lua
+++ lua/custom/plugins/vim-dadbod-ui.lua
@@ -16,5 +16,18 @@ return {
     vim.g.db_ui_disable_progress_bar = 0
     vim.g.db_ui_use_nerd_fonts = 1
     vim.g.db_ui_winwidth = 80
-  end,
+    vim.g.db_ui_save_location = vim.fn.stdpath('data') .. '/dadbod'
+
+    vim.g.dbs = {
+      { name = 'TREE DB Forestry',
+        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_TEST_PASSWORD .. '@' .. vim.env.TREEDOM_DB_TREES_TEST_URL .. ':' .. vim.env.TREEDOM_DB_PORT .. '/'
+      },
+      { name = 'TEST',
+        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_TEST_PASSWORD .. '@' .. '127.0.0.1:3306/forestry'
+      },
+      { name = 'Trees-prod',
+        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_PROD_PASSWORD .. '@' .. vim.env.TREEDOM_DB_TREES_PROD_URL .. ':' .. vim.env.TREEDOM_DB_PORT .. '/'
+      },
+    }
+  end
 }
