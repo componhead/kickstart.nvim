5f79d116ed68a5c6adf1b875defbeff433655646
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-10-25 18:49:36 +0200

    add markdown parser

--- lua/custom/plugins/vim-dadbod-ui.lua
+++ lua/custom/plugins/vim-dadbod-ui.lua
@@ -18,22 +18,24 @@ return {
     vim.g.db_ui_winwidth = 80
     vim.g.db_ui_save_location = vim.fn.stdpath('data') .. '/dadbod'
 
-    vim.g.dbs = {
-      { name = 'TUNNELLED (w/o password)',
-        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. '@' .. '127.0.0.1:3306/'
-      },
-      { name = 'TREE DB TEST',
-        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_TEST_PASSWORD .. '@' .. '127.0.0.1:7001/'
-      },
-      { name = 'TREES DB PROD',
-        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_PROD_PASSWORD .. '@' .. '127.0.0.1:7011'
-      },
-      { name = 'TREEDOM DB TEST',
-        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TRD_TEST_PASSWORD .. '@' .. '127.0.0.1:7002/'
-      },
-      { name = 'TREESDOM DB PROD',
-        url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TRD_PROD_PASSWORD .. '@' .. '127.0.0.1:7022'
-      },
-    }
+    if vim.env.TREEDOM_DB_USER ~= nil then
+      vim.g.dbs = {
+        { name = 'TUNNELLED (w/o password)',
+          url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. '@' .. '127.0.0.1:3306/'
+        },
+        { name = 'TREE DB TEST',
+          url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_TEST_PASSWORD .. '@' .. '127.0.0.1:7001/'
+        },
+        { name = 'TREES DB PROD',
+          url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TREES_PROD_PASSWORD .. '@' .. '127.0.0.1:7011'
+        },
+        { name = 'TREEDOM DB TEST',
+          url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TRD_TEST_PASSWORD .. '@' .. '127.0.0.1:7002/'
+        },
+        { name = 'TREESDOM DB PROD',
+          url =  'mysql://' .. vim.env.TREEDOM_DB_USER .. ':' .. vim.env.TREEDOM_DB_TRD_PROD_PASSWORD .. '@' .. '127.0.0.1:7022'
+        },
+      }
+    end
   end
 }
