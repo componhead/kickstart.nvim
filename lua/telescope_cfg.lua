7c4c49b8e9b5823ed56a08c951ace1fd77b48d14
Emiliano Anichini <emiliano.anichini@gmail.com>
2024-12-20 09:08:23 +0100

    readd telecope-native

--- lua/telescope_cfg.lua
+++ lua/telescope_cfg.lua
@@ -4,6 +4,19 @@ return { -- Fuzzy Finder (files, lsp, etc)
   branch = '0.1.x',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    { -- If encountering errors, see telescope-fzf-native README for installation instructions
+      'nvim-telescope/telescope-fzf-native.nvim',
+
+      -- `build` is used to run some command when the plugin is installed/updated.
+      -- This is only run then, not every time Neovim starts up.
+      build = 'make',
+
+      -- `cond` is a condition used to determine whether this plugin should be
+      -- installed and loaded.
+      cond = function()
+        return vim.fn.executable 'make' == 1
+      end,
+    },
     { 'nvim-telescope/telescope-ui-select.nvim' },
 
     -- Useful for getting pretty icons, but requires a Nerd Font.
@@ -496,6 +509,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sz', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = 'fuzzy search in current buffer' })
 
     -- Enable Telescope extensions if they are installed
+    pcall(require('telescope').load_extension, 'fzf')
     pcall(require('telescope').load_extension, 'ui-select')
     pcall(require('telescope').load_extension, 'neoclip')
     pcall(require("telescope").load_extension, 'persisted')
